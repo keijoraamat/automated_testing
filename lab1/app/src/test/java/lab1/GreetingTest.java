3a3b7c522165bae6d2db33719c5275194ca9a4f1
Keijo Raamat <raamatkeijo@gmail.com>
2022-09-13 15:25:56 +0300

    Adds tests for REQ 2

--- lab1/app/src/test/java/lab1/GreetingTest.java
+++ lab1/app/src/test/java/lab1/GreetingTest.java
@@ -6,6 +6,8 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 public class GreetingTest {
 
+    public static String NAMELESS = "my friend";
+
     @Test
     public void should_return_name_in_greeting(){
         // Arrenge
@@ -19,5 +21,48 @@ public class GreetingTest {
         // Assert
         assertThat(actualResult).isEqualTo(expectedResult);
 
+    }
+
+
+    @Test
+    public void should_return_stand_in_when_empty_string_is_given(){
+        // Arrange
+        String name = "";
+        Greeting greeting = new Greeting();
+        String expectedResult = String.format("Hello, %s", NAMELESS);
+
+        // Act
+        String actualResult = greeting.greet(name);
+
+        // Assert
+        assertThat(actualResult).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void should_return_stand_in_when_name_is_null(){
+        // Arrange
+        String name = null;
+        Greeting greeting = new Greeting();
+        String expectedResult = String.format("Hello, %s", NAMELESS);
+
+        // Act
+        String actualResult = greeting.greet(name);
+
+        // Assert
+        assertThat(actualResult).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void should_return_stand_in_when_name_is_whitespaces(){
+        // Arrange
+        String name = "            ";
+        Greeting greeting = new Greeting();
+        String expectedResult = String.format("Hello, %s", NAMELESS);
+
+        // Act
+        String actualResult = greeting.greet(name);
+
+        // Assert
+        assertThat(actualResult).isEqualTo(expectedResult);
     }
 }
