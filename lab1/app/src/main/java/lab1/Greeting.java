4e064a601d0cc244d278053bb4a17a250f80acca
Keijo Raamat <raamatkeijo@gmail.com>
2022-09-13 15:38:32 +0300

    Implements code for REQ 2

--- lab1/app/src/main/java/lab1/Greeting.java
+++ lab1/app/src/main/java/lab1/Greeting.java
@@ -1,8 +1,22 @@
 package lab1;
 
 public class Greeting {
+
+    public static String NAMELESS = "my friend";
     
     public String greet(String name){
+
+        if (name == null) { 
+            return String.format("Hello, %s", NAMELESS);
+        }
+
+        else if (name.length() == 0) {
+            return String.format("Hello, %s", NAMELESS);
+        }
+
+        else if (name.trim().length() == 0) {
+            return String.format("Hello, %s", NAMELESS);
+        }
 
         return String.format("Hello, %s", name);
     }
