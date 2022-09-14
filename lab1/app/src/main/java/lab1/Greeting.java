babe01d7c249c3632a5eacd19e2571418d6da5c6
Keijo Raamat <raamatkeijo@gmail.com>
2022-09-14 20:56:37 +0300

    Refactos REQ 3 code

--- lab1/app/src/main/java/lab1/Greeting.java
+++ lab1/app/src/main/java/lab1/Greeting.java
@@ -5,21 +5,22 @@ import org.apache.commons.lang3.StringUtils;
 public class Greeting {
 
     public static String NAMELESS = "my friend";
+    public static String GREETING = "Hello";
     
     public String greet(String name){
 
         if (name == null) { 
-            return String.format("Hello, %s", NAMELESS);
+            return String.format("%s, %s", GREETING, NAMELESS);
         }
 
         else if (name.trim().length() == 0) {
-            return String.format("Hello, %s", NAMELESS);
+            return String.format("%s, %s", GREETING, NAMELESS);
         }
 
         else if (StringUtils.isAllUpperCase(name)) {
-            return String.format("HELLO, %s", name);
+            return String.format("%s, %s", GREETING.toUpperCase(), name);
         }
 
-        return String.format("Hello, %s", name);
+        return String.format("%s, %s", GREETING, name);
     }
 }
